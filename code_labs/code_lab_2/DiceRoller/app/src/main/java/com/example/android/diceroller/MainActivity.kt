a281f5f5ed400fe844398c50b7d8009bca6c27aa
neoNite77 <abrahamdaungos@gmail.com>
2023-01-30 21:31:24 -0800

    Dice Roller Code Lab 3

--- code_labs/code_lab_2/DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ code_labs/code_lab_2/DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     // The lateinit keyword promises the Kotlin compiler that
     //the variable will be initialized before the code calls any operations on it
     lateinit var diceImage : ImageView
+    lateinit var diceImage2 : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,36 +28,42 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
-        val countUpButton: Button = findViewById(R.id.count_up_button)
-        countUpButton.setOnClickListener { countUpNumber() }
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
 
-        val resetButton: Button = findViewById(R.id.reset_button)
-        resetButton.setOnClickListener { resetCount() }
+//        val countUpButton: Button = findViewById(R.id.count_up_button)
+//        countUpButton.setOnClickListener { countUpNumber() }
+//
+//        val resetButton: Button = findViewById(R.id.reset_button)
+//        resetButton.setOnClickListener { resetCount() }
     }
 
     /**
      * Click listener for the Roll button
      */
     private fun rollDice() {
-        val randomInt = (1..6).random()
+
         Toast.makeText(this, "Roll Dice",
             Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
-        //resultText.text = getString(R.string.dice_rolled_output)
+//        val resultText: TextView = findViewById(R.id.result_text)
+//        resultText.text = randomInt.toString()
+//        resultText.text = getString(R.string.dice_rolled_output)
 
-        // Get the ImageView Id and assign a picture depending of the value 1-6
-//        val diceImage : ImageView = findViewById(R.id.dice_image)
-//        val drawableResource = when (randomInt) {
-//            1 -> R.drawable.dice_1
-//            2 -> R.drawable.dice_2
-//            3 -> R.drawable.dice_3
-//            4 -> R.drawable.dice_4
-//            5 -> R.drawable.dice_5
-//            else -> R.drawable.dice_6
-//        }
-//
-//        diceImage.setImageResource(drawableResource)
+        //Get the ImageView Id and assign a picture depending of the value 1-6
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
+
+    private fun getRandomDiceImage() : Int {
+        val randomInt = (1..6).random()
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
     }
 
     /**
